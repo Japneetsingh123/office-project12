497c206d6e35a51865abb4dad1b32f7ed06f8ba6
Japneet Singh Chadha <japneetsingh560@gmail.com>
2023-11-17 18:49:36 +0530

    Add files via upload

---
+++ src/Formsignin.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import validate from './validateinfo';
+import useForm from './useForm';
+import './Form.css';
+import FormSignup from './FormSignup';
+import {NavLink} from 'react-router-dom';
+const FormSignin = ({ submitForm }) => {
+  const { handleChange, handleSubmit, values, errors } = useForm(
+    submitForm,
+    validate
+  );
+
+  return (
+    <div className='form-content-right'>
+      <form onSubmit={handleSubmit} className='form' noValidate>
+        <h1>
+          Hey user. Login your details . 
+        </h1>
+        <div className='form-inputs'>
+          <label className='form-label'>Username</label>
+          <input
+            className='form-input'
+            type='text'
+            name='username'
+            placeholder='Enter your username'
+            value={values.username}
+            onChange={handleChange}
+          />
+          {errors.username && <p>{errors.username}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Email</label>
+          <input
+            className='form-input'
+            type='email'
+            name='email'
+            placeholder='Enter your email'
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password'
+            placeholder='Enter your password'
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Confirm Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='conpassword'
+            placeholder='Confirm your password'
+            value={values.conpassword}
+            onChange={handleChange}
+          />
+          {errors.conpassword && <p>{errors.conpassword}</p>}
+        </div>
+        
+       
+      </form>
+    </div>
+  );
+};
+
+export default FormSignin;
