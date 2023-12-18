a324ad250d867ccf68847428909191f79400957a
Japneet Singh Chadha <japneetsingh560@gmail.com>
2023-12-18 16:21:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# office-project12
+# React + Vite
 
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Start the development server by running the following command:
-npm run build
-npm start
-You are now ready to go!
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/register.jsx
@@ -0,0 +1,126 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import '../src/login'
+import '/projects/my-app/vite-app/src/App.css';
+const AddEmployee = () => {
+  const [employee, setEmployee] = useState({
+    name: "",
+    email: "",
+    password: "",
+    connpassword:""
+  });
+  const [category, setCategory] = useState([]);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5173/register.jsx")
+      .then((result) => {
+        if (result.data.Status) {
+          setCategory(result.data.Result);
+        } else {
+          alert(result.data.Error);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData();
+    formData.append('name', employee.name);
+    formData.append('email', employee.email);
+    formData.append('password', employee.password);
+    formData.append('address', employee.connpassword);
+    
+
+    axios.post('http://localhost:5173/register.jsx', formData)
+    .then(result => {
+        if(result.data.Status) {
+            navigate('../login.jsx')
+        } else {
+            alert(result.data.Error)
+        }
+    })
+    .catch(err => console.log(err))
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center mt-3">
+      <div className="p-3 rounded w-50 border">
+        <h3 className="text-center">Registration page</h3>
+        <form className="row g-1" onSubmit={handleSubmit}>
+          <div className="col-12">
+            <label for="inputName" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputName"
+              placeholder="Enter Name"
+              onChange={(e) =>
+                setEmployee({ ...employee, name: e.target.value })
+              }
+            />
+          </div>
+          <div className="col-12">
+            <label for="inputEmail4" className="form-label">
+              Email id
+            </label>
+            <input
+              type="email"
+              className="form-control rounded-0"
+              id="inputEmail4"
+              placeholder="Enter Email"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="col-12">
+            <label for="inputPassword4" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control rounded-0"
+              id="inputPassword4"
+              placeholder="Enter Password"
+              onChange={(e) =>
+                setEmployee({ ...employee, password: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label for="inputSalary" className="form-label">
+              confirm Password
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputPassword4"
+              placeholder="Enter Confirm password"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, connpassword: e.target.value })
+              }
+            />
+          </div>
+          
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary w-100" onClick={alert('data submitted')}>
+              Submit Information
+            </button>
+          </div>
+          <div>to login click on <Link to='../login.jsx'><span className='font-bold'>
+            Login Now</span></Link> </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmployee;
